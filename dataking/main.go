0ef0e527b7319c71eb0bad8ad7cda7c4e890e6e6
Tim Paduraru <timopaduraru@gmail.com>
2022-06-12 06:08:09 -0700

    increased window size

--- dataking/main.go
+++ dataking/main.go
@@ -48,7 +48,7 @@ func main() {
 	if runtime.GOOS == "linux" {
 		args = append(args, "--class=Lorca")
 	}
-	ui, err := lorca.New("", "", 480, 320, args...)
+	ui, err := lorca.New("", "", 680, 520, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
